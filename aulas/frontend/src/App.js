8397219325dfc63726d74d0bbd91ededb0677a71
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2020-03-25 22:36:43 -0300

    Add files via upload

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,27 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes/>
   );
 }
 
 export default App;
+/*
+  function App() {
+    let [counter, setCounter] = useState(0);
+
+    function increment() {
+      setCounter(counter += 1);
+    }
+
+    return (
+      <div>
+        <Header>Contador: {counter}</Header>
+        <button onClick={increment}>Incrementar</button>
+      </div>
+    );
+  }
+*/
+
+/*
+  function App() {
+    return (
+      <Header title="Semana OmniStack">
+        Semana OmniStack
+      </Header>
+    );
+  }
+*/
+
+
 // Link de acesso -> localhost:3000
